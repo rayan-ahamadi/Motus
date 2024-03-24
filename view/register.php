00d67bc0ec70923dd815ccdba585c16016fdbfca
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-03-24 01:06:20 +0100

    Ajout de meta description

--- view/register.php
+++ view/register.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Jeu Motus, réalisé par Rayan.A">
   <link rel="stylesheet" href="style.css">
   <link rel="icon" href="lettre-m.png" />
   <title>Motus</title>
